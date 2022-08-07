73c58f807d7249246714a564667a96c8c0faae51
BossBele <adambeleko@gmail.com>
2022-08-07 22:27:44 +0300

    server api

--- src/server.js
+++ src/server.js
@@ -13,46 +13,62 @@ export default function MailerAPI(settings = defaultSettings) {
 
   const MAIL_CONFIG = new Config(options)
   const transporter = nodemailer.createTransport(MAIL_CONFIG)
-  // Server-side mailing API
-  /**
-   * @function mail
-   * @param {MailBody} config
-   * @returns {Promise<any>}
-   */
-  return async function mailer({
-    data,
-    html,
-    receivers,
-    sender,
-    subject,
-    text
-  }) {
-    try {
-      const mailOptions = {
-        from: `"${sender}" <${MAIL_CONFIG.auth.user}>`, // sender address
-        to: `${receivers}`, // list of receivers
-        subject, // Subject line
-        text, // text body
-        html // html body
-      }
 
-      if (!sender) {
-        throw new Error('Sender not set')
-      }
+  const body = {}
 
-      if (Object.prototype.hasOwnProperty.call(data, 'file')) {
-        mailOptions.attachments = [
-          {
-            filename: `${data.filePath[0]}`,
-            path: `${data.file[0]}`
-          }
-        ]
-      }
+  return mailer(
+    // {
+    //   data,
+    //   html,
+    //   receivers,
+    //   sender,
+    //   subject,
+    //   text
+    // },
+    body,
+    MAIL_CONFIG,
+    logger,
+    transporter
+  )
+}
 
-      // call of this function send an email, and return status
-      transporter.sendMail(mailOptions, (error, info) =>
-        getDeliveryStatus(error, info, logger)
-      )
-    } catch (error) {}
-  }
+// Server-side mailing API
+/**
+ * @function mail
+ * @param {MailBody} config
+ * @returns {Promise<any>}
+ */
+async function mailer(
+  { data, html, receivers, sender, subject, text },
+  MAIL_CONFIG,
+  logger,
+  transporter
+) {
+  try {
+    const mailOptions = {
+      from: `"${sender}" <${MAIL_CONFIG.auth.user}>`, // sender address
+      to: `${receivers}`, // list of receivers
+      subject, // Subject line
+      text, // text body
+      html // html body
+    }
+
+    if (!sender) {
+      throw new Error('Sender not set')
+    }
+
+    if (Object.prototype.hasOwnProperty.call(data, 'file')) {
+      mailOptions.attachments = [
+        {
+          filename: `${data.filePath[0]}`,
+          path: `${data.file[0]}`
+        }
+      ]
+    }
+
+    // call of this function send an email, and return status
+    transporter.sendMail(mailOptions, (error, info) =>
+      getDeliveryStatus(error, info, logger)
+    )
+  } catch (error) {}
 }
