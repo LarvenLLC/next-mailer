249c817825b24c9fe09e0a24b9271ee40a98a8a8
BossBele <adambeleko@gmail.com>
2022-08-07 10:42:16 +0300

    init

---
+++ src/server.js
@@ -0,0 +1,56 @@
+import Config from './Config'
+import { getDeliveryStatus } from './helper'
+const nodemailer = require('nodemailer')
+
+/**
+ *
+ * @param {object} config
+ * @param {Logger} config.logger
+ * @returns
+ */
+export default function MailerAPI({ logger, ...options }) {
+  const MAIL_CONFIG = new Config(options)
+  const transporter = nodemailer.createTransport(MAIL_CONFIG)
+  // Server-side mailing API
+  /**
+   * @function mail
+   * @param {MailBody} config
+   * @returns {Promise<any>}
+   */
+  return async function mailer({
+    data,
+    html,
+    receivers,
+    sender,
+    subject,
+    text
+  }) {
+    try {
+      const mailOptions = {
+        from: `"${sender}" <${MAIL_CONFIG.auth.user}>`, // sender address
+        to: `${receivers}`, // list of receivers
+        subject, // Subject line
+        text, // text body
+        html // html body
+      }
+
+      if (!sender) {
+        throw new Error('Sender not set')
+      }
+
+      if (Object.prototype.hasOwnProperty.call(data, 'file')) {
+        mailOptions.attachments = [
+          {
+            filename: `${data.filePath[0]}`,
+            path: `${data.file[0]}`
+          }
+        ]
+      }
+
+      // call of this function send an email, and return status
+      transporter.sendMail(mailOptions, (error, info) =>
+        getDeliveryStatus(error, info, logger)
+      )
+    } catch (error) {}
+  }
+}
