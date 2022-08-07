fc7eb18a9bd479acccfb581fb9e04bfa907ae5a4
BossBele <adambeleko@gmail.com>
2022-08-07 10:41:55 +0300

    init

--- src/client.js
+++ src/client.js
@@ -1,38 +1,19 @@
-// Client-side logging API
-async function log(message, attributes, type = 'info') {
-  const response = await fetch(`/api/logger/${type}`, {
+// Client-side mailing API
+/**
+ * @function mail
+ * @param {MailBody} config
+ * @returns {Promise<any>}
+ */
+async function mail(config) {
+  const response = await fetch(`/api/mailer`, {
     method: 'POST',
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({ message, attributes })
+    body: JSON.stringify(config)
   })
   return await response.json()
 }
 
-const clientLog = {
-  log,
-  info: (message = '', attributes = {}) => log(message, attributes, 'info'),
-  debug: (message = '', attributes = {}) => log(message, attributes, 'debug'),
-  warn: (message = '', attributes = {}) => log(message, attributes, 'warn'),
-  error: (message = '', attributes = {}) => log(message, attributes, 'error')
-}
-export default clientLog
-
-// Client-side log reading API
-async function read(type = 'console') {
-  const response = await fetch(`/api/logger/${type}`, {
-    method: 'GET'
-  })
-  return await response.text()
-}
-
-const clientRead = {
-  read,
-  info: () => read('info'),
-  debug: () => read('debug'),
-  warn: () => read('warn'),
-  error: () => read('error')
-}
-export { clientRead }
+export default mail
