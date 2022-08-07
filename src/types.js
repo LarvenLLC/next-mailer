1f3967a2e980a3fa83d004117c45fde554d9afc0
BossBele <adambeleko@gmail.com>
2022-08-07 10:42:18 +0300

    init

---
+++ src/types.js
@@ -0,0 +1,17 @@
+/**
+ * @typedef {object} MailBody
+ * @property {object} data
+ * @property {string} html
+ * @property {string} receivers
+ * @property {string} sender
+ * @property {string} subject
+ * @property {string} text
+ */
+
+/**
+ * @typedef {object} Logger
+ * @property {function()} debug
+ * @property {function()} error
+ * @property {function()} info
+ * @property {function()} warn
+ */
