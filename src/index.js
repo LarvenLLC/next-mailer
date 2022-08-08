b2b2092f42ecfcae278333cccdbcbb3f18009040
BossBele <adambeleko@gmail.com>
2022-08-08 18:09:26 +0300

    separate client side api

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import NextMailer from './api'
-import mail from './client'
 import MailerAPI from './server'
 
-module.exports = mail
 module.exports.NextMailer = NextMailer
 module.exports.Mailer = MailerAPI
