0f46e23f69589d2229eba46b7c6d1048079b5b65
BossBele <adambeleko@gmail.com>
2022-08-08 18:37:13 +0300

    separate client from server api

--- src/react/index.js
+++ src/react/index.js
@@ -1,19 +1,3 @@
-// Client-side mailing API
-/**
- * @function mail
- * @param {MailBody} config
- * @returns {Promise<any>}
- */
-async function mail(config) {
-  const response = await fetch(`/api/mailer`, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(config)
-  })
-  return await response.json()
-}
+import mail from './client'
 
-export default mail
+module.exports = mail
