c765bb0f9c92c6a2e12e7565fa30b3b459bbb08c
BossBele <adambeleko@gmail.com>
2022-08-11 18:40:38 +0300

    update api

---
+++ src/react/types.js
@@ -0,0 +1,9 @@
+/**
+ * @typedef {object} MailBody
+ * @property {object} data
+ * @property {string} html
+ * @property {string} receivers
+ * @property {string} sender
+ * @property {string} subject
+ * @property {string} text
+ */
