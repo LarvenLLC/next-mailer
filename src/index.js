4af8257fce7f5d16bbdbb849ce85b26c80a1584e
Adam Beleko <adambeleko@gmail.com>
2023-08-11 19:07:13 +0300

    fix: mailer server function - src/index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import NextMailer, { getTransporter } from './api'
+import NextMailer from './api'
 import mailer from './server'
 
 module.exports = NextMailer
-module.exports.getTransporter = getTransporter
 module.exports.mailer = mailer
